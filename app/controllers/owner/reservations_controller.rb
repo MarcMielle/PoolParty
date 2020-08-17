d326422c71a5e6d997f91dd917206361b38a3f8c
MarcMielle <marcmielle96@gmail.com>
2020-08-17 17:24:52 +0200

    Routes Created

---
+++ app/controllers/owner/reservations_controller.rb
@@ -0,0 +1,2 @@
+class Owner::ReservationsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :swimming_pools, only: [:index, :show, :new, :create] do
+    resources :reservations, only: [:new, :create]
+  end
+
+  resources :reservations, only: [:index]
+
+  namespace :owner do
+    resources :reservations, only: [:index] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
