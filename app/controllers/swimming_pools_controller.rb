8c11357292a0d5bbf221a5b2f05f2791af7a65a4
Nicolas Hubert <nicolas.hubert@eleves.ec-nantes.fr>
2020-08-18 13:17:57 +0200

    User displays an offer

--- app/controllers/swimming_pools_controller.rb
+++ app/controllers/swimming_pools_controller.rb
@@ -1,4 +1,7 @@
 class SwimmingPoolsController < ApplicationController
+
+  # GET /swimming_pool/1
   def show
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,2 @@
 class UsersController < ApplicationController
-
-  # GET /swimming_pool/1
-  def show
-  end
-
 end
