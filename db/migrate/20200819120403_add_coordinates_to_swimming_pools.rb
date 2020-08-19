f1df5ff4d6c2cc3df033db7486eedba18592bfc1
Nicolas Hubert <nicolas.hubert@eleves.ec-nantes.fr>
2020-08-19 14:33:21 +0200

    Add map

--- db/migrate/20200819120403_add_coordinates_to_swimming_pools.rb
+++ db/migrate/20200819120403_add_coordinates_to_swimming_pools.rb
@@ -1,4 +1,4 @@
-class AddCoordinatesTo < ActiveRecord::Migration[6.0]
+class AddCoordinatesToSwimmingPools < ActiveRecord::Migration[6.0]
   def change
     add_column :swimming_pools, :latitude, :float
     add_column :swimming_pools, :longitude, :float
