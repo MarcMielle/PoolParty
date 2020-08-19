c43c6c5fa4fa303cb19a8f740c9a4671faea2486
Nicolas Hubert <nicolas.hubert@eleves.ec-nantes.fr>
2020-08-19 11:36:30 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/swimming_pools_controller.rb
+++ app/controllers/swimming_pools_controller.rb
@@ -1,12 +1,23 @@
 class SwimmingPoolsController < ApplicationController
 
+  def index
+    @swimming_pools = SwimmingPool.geocoded # returns swimming_pools with coordinates
+
+    @markers = @swimming_pools.map do |swimming_pool|
+      {
+        lat: swimming_pool.latitude,
+        lng: swimming_pool.longitude
+      }
+    end
+  end
+
   # GET /swimming_pool/1
   def show
     @swimming_pool = SwimmingPool.find(params[:id])
   end
 
-  def index
-    @swimmingpools = SwimmingPool.all
-  end
+  # def index
+  #   @swimmingpools = SwimmingPool.all
+  # end
 
 end
