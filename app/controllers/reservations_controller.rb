dc7d9af2cd42148014549fc762e71922b52cf55e
Marion Debonnaire <marion.debonnaire@gmail.com>
2020-08-19 15:18:53 +0200

    Add reservation form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,4 +3,22 @@ class ReservationsController < ApplicationController
     # Reservations where le user est == au current_user
     @reservations = current_user.reservations
   end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
 end
