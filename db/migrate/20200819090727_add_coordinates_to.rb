e8d4c8068730c72b1d8ee84ad95769a365895424
MarcMielle <marcmielle96@gmail.com>
2020-08-19 16:26:21 +0200

    Fixing migrations

--- db/migrate/20200819090727_add_coordinates_to.rb
+++ db/migrate/20200819090727_add_coordinates_to.rb
@@ -1,4 +1,6 @@
 class AddCoordinatesTo < ActiveRecord::Migration[6.0]
   def change
+    add_column :swimming_pools, :latitude, :float
+    add_column :swimming_pools, :longitude, :float
   end
 end
--- db/migrate/20200819120403_add_coordinates_to_swimming_pools.rb
+++ db/migrate/20200819120403_add_coordinates_to_swimming_pools.rb
@@ -1,6 +1,4 @@
 class AddCoordinatesToSwimmingPools < ActiveRecord::Migration[6.0]
   def change
-    add_column :swimming_pools, :latitude, :float
-    add_column :swimming_pools, :longitude, :float
   end
 end
